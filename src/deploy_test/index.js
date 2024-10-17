93010d56c7ab2b34cbd3e2e9476357194ede61eb
dreamoKane <kane@dream-o.com>
2024-10-17 14:50:16 +0900

    Revert "deploy_test 삭제"
    This reverts commit 52820c480f23658ce8fb846ca1263634f22b1012.

---
+++ src/deploy_test/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+// 기본 라우트 설정
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+// Cloud Functions에서 사용할 엔트리 포인트 함수
+exports.deploy_test = (req, res) => {
+    app(req, res);
+};
