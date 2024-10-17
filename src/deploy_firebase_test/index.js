082a0ecae878ae85cfef5ffb7114402d90be4376
dreamoKane <kane@dream-o.com>
2024-10-17 18:14:00 +0900

    수정

--- src/deploy_firebase_test/index.js
+++ src/deploy_firebase_test/index.js
@@ -1,5 +1,5 @@
 const functions = require('firebase-functions/v1');
 
 exports.deploy_firebase_test = functions.https.onRequest((req, res) => {
-    res.send('Hello World! My name is Kane.!');
+    res.send('Hello World! My name is Kane.!!!!');
 });
