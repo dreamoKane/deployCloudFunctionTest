70a2114bdf7539cdedb14ce20a7ceff2536572b8
dreamoKane <151102413+dreamoKane@users.noreply.github.com>
2024-10-17 11:56:33 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,2 +1,11 @@
-def hello_world(request):
-    return "Hello World!"
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def hello_world():
+    return "Hello World!", 200
+
+# Cloud Functions가 호출할 수 있도록 main 함수 추가
+def hello_world_handler(request):
+    return app(request)
