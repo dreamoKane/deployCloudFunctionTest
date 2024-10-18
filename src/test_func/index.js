c86c997c24774d61929190e2d291c5fbd4d5b463
dreamoKane <kane@dream-o.com>
2024-10-18 16:55:47 +0900

    문구 변경

--- src/test_func/index.js
+++ src/test_func/index.js
@@ -3,7 +3,7 @@ const app = express();
 
 // 기본 라우트 설정
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello World!! hi hi hi');
 });
 
 // Cloud Functions에서 사용할 엔트리 포인트 함수
