cd5490f9c344de4bdc743e95fdfc3f260335af09
dreamoKane <kane@dream-o.com>
2024-10-29 15:43:54 +0900

    test

--- src/test_cloud_function.js
+++ src/test_cloud_function.js
@@ -1,5 +1,5 @@
 const functions = require('firebase-functions/v1');
 
-exports.test_func = functions.https.onRequest((req, res) => {
+exports.test_cloud_function = functions.https.onRequest((req, res) => {
     res.send('Hello World! My name is Kane!!');
 });
